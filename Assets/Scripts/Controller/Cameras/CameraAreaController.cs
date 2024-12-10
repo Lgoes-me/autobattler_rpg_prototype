b2ff9e985e102700b1dc47b252b4bcd9ff7492a1
Góes <lucas.rodrigues.goes@gmail.com>
2024-12-10 00:19:46 -0300

    fix follow

--- Assets/Scripts/Controller/Cameras/CameraAreaController.cs
+++ Assets/Scripts/Controller/Cameras/CameraAreaController.cs
@@ -5,9 +5,10 @@ public class CameraAreaController : MonoBehaviour
 {
     [field: SerializeField] private CinemachineVirtualCamera CinemachineVirtualCamera { get; set; }
     [field: SerializeField] private CinemachineBlendDefinition Blend { get; set; }
+    [field: SerializeField] private bool Follow { get; set; }
 
     private CinemachineBrain CinemachineBrain { get; set; }
-    
+
     private void Awake()
     {
         CinemachineBrain = Application.Instance.MainCamera.transform.GetComponent<CinemachineBrain>();
@@ -27,12 +28,18 @@ public class CameraAreaController : MonoBehaviour
         {
             CinemachineBrain.m_DefaultBlend = Blend;
             CinemachineVirtualCamera.Priority = 0;
-        } 
+        }
     }
 
     public void ActivateCamera()
     {
         Application.Instance.InputManager.SetNewCameraPosition(CinemachineVirtualCamera.transform);
+        
+        if (Follow)
+        {
+            CinemachineVirtualCamera.Follow = Application.Instance.PlayerManager.PlayerController.transform;
+        }
+
         CinemachineVirtualCamera.Priority = 10;
     }
 }
