97fab41d55efa76fad64bc2fedcf4f3f4527ff8e
Góes <lucas.rodrigues.goes@gmail.com>
2025-01-07 00:55:34 -0300

    Starting to refactor cutscenes

---
+++ Assets/Scripts/Data/CutsceneData.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class CutsceneData : ScriptableObject
+{
+    [field: SerializeField] private DialogueData Dialogue { get; set; }
+    [field: SerializeReference] [field: SerializeField] private GameAction EndCutsceneAction { get; set; }
+}
