db7456ce244a906afce152cfdf410d4c25ca61b8
Góes <lucas.rodrigues.goes@gmail.com>
2025-04-26 19:19:47 -0300

    Modificando AoE

---
+++ Assets/Scripts/Data/Abilities/Action/AreaOfEffectActionData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class AreaOfEffectActionData : ActionData
+{
+    [field: SerializeField] private float Range { get; set; }
+    
+    public override AbilityActionComponent ToDomain(
+        PawnController abilityUser,
+        List<AbilityEffect> effects,
+        AbilityFocusComponent abilityFocusComponent)
+    {
+        return new AreaOfEffectActionComponent(abilityUser, effects, abilityFocusComponent, Range);
+    }
+}
---
+++ Assets/Scripts/Domain/Abilities/Action/AreaOfEffectActionComponent.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class AreaOfEffectActionComponent : AbilityActionComponent
+{
+    private float Range { get; set; }
+    
+    public AreaOfEffectActionComponent(
+        PawnController abilityUser, 
+        List<AbilityEffect> effects,
+        AbilityFocusComponent abilityFocusComponent,
+        float range) : base(abilityUser, effects, abilityFocusComponent)
+    {
+        Range = range;
+    }
+
+    public override void DoAction()
+    {
+        var closePawns = AbilityUser.Battle.Pawns
+            .Where(p => 
+                p.Pawn.Team != AbilityUser.Pawn.Team && 
+                Vector3.Distance(AbilityUser.transform.position, p.transform.position) < Range)
+            .ToList();
+
+        foreach (var pawn in closePawns)
+        {
+            foreach (var effect in Effects)
+            {
+                effect.DoAbilityEffect(pawn);
+            }
+        }
+    }
+}
